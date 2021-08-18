88a1d981469016895fd7b68e0c6e06bc2bb18956
carolinarodrigues21 <carolina.nmrr@gmail.com>
2021-08-18 19:51:56 -0300

    comments

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#HttpREsponse specific view function
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
