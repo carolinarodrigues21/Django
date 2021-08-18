f4a47ebd4ba923501fa1087bea6d913d87397127
carolinarodrigues21 <carolina.nmrr@gmail.com>
2021-08-18 20:14:21 -0300

    created models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+#modes.___ -> type of data each field holds 
+class Question(models.Model):
+    #variables name will be used as the database column's name
+    question_text = models.CharField(max_lenght = 200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
