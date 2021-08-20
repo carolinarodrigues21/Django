c531756ed1f7d9540a0f1bfef819e925a65bc1d4
carolinarodrigues21 <carolina.nmrr@gmail.com>
2021-08-20 12:58:37 -0300

    changed views with template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
-#HttpREsponse specific view function
+#HttpResponse specific view function
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('polls/index.htm')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("YOu're looking at question %s." % question_id)
