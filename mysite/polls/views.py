4faaa536ae4dd664557ccf039b4ba8ced84b98c9
carolinarodrigues21 <carolina.nmrr@gmail.com>
2021-08-20 12:46:48 -0300

    altered the display

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 #HttpREsponse specific view function
 
 def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
