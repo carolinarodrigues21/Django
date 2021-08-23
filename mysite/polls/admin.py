3e9750b9578d9267aaad8e74d5102c516bc2ead6
carolinarodrigues21 <carolina.nmrr@gmail.com>
2021-08-23 12:59:59 -0300

    added choices to questions

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice 
+    extra = 3
 class QuestionAdmin(admin.ModelAdmin):
 
     fieldsets = [
 
         #title f the field set, field info
         (None,                {'fields': ['question_text']}),
-        ('Date informaition', {'fields':['pub_date']}),
+        ('Date informaition', {'fields':['pub_date'], 'classes':['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
+
