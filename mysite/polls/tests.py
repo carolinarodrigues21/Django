9c064048eb90fd4df801a8e4b2f2dc342df2fc52
carolinarodrigues21 <carolina.nmrr@gmail.com>
2021-08-23 00:31:32 -0300

    running first test

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -9,6 +9,9 @@ from .models import Question
 
 class QuestionModelTests(TestCase):
 
+    #tests are recognized as those whose name begins with test
+
+    #The test informs us which test failed and even the line on which the failure occurred.
     def test_was_published_recently_with_future_question(self):
 
         """
@@ -18,6 +21,6 @@ class QuestionModelTests(TestCase):
         """
 
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
+        future_question = Question(pub_date=time)                       #creates a Question instance whose pub_date field is 30 days in the future
         self.assertIs(future_question.was_published_recently(), False)
-        
+
