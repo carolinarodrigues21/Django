50176459cdad9eeec34e63955dde175ca51a91e6
carolinarodrigues21 <carolina.nmrr@gmail.com>
2021-08-23 13:50:06 -0300

    customized projects template

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],                               #list of filesystem directories to check when loading Django templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
