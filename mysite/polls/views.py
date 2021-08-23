2fdd57af8422bcbe336dce19be49e672b14c8ab7
carolinarodrigues21 <carolina.nmrr@gmail.com>
2021-08-23 01:15:17 -0300

    added views and tests for it

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question 
-
-#HttpResponse specific view function
 
 #subsistute the previous functions
 class IndexView(generic.ListView):
@@ -38,3 +37,14 @@ def vote(request, question_id):
         selected_choice.votes +=1 
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def get_queryset(self):
+
+    """
+    Return the last 5 published questions with pub_date <= timezone.now()
+    (not including those set to be published in the future)
+
+    """
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
